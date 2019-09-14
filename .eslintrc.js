b56164f1e6a88b901c1c1dfc44d83d240466760c
Edo Freriks <edofre@gmail.com>
2019-09-15 00:29:00 +0200

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,27 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1
+    }],
+    'vue/html-indent': ['error', 2, {
+      'attribute': 1,
+      'baseIndent': 1,
+      'closeBracket': 0,
+      'alignAttributesVertically': true,
+      'ignores': []
+    }]
   },
+  'overrides': [
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
+      }
+    }
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   }
