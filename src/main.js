32015995288577200ac38f12f3ed2c12d8d1bf5a
Edo Freriks <edofre@gmail.com>
2019-09-14 23:42:56 +0200

    AFAS axios header

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+// Axios
+import axios from 'axios'
+
+axios.defaults.headers.get['Accepts'] = 'application/json'
+// Add AFAS token if we have it
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = 'AfasToken ' + token
+  }
+  return config
+})
 
 Vue.config.productionTip = false
 
