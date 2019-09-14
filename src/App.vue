a677b06599c591c0aef7d3b2eba6e5da8908d935
Edo Freriks <edofre@gmail.com>
2019-09-15 00:48:01 +0200

    Token.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
     }
 
     #nav {
-        padding: 30px;
+        padding: 15px;
     }
 
     #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld></HelloWorld>
+        <app-token></app-token>
     </div>
 </template>
 
 <script>
   // @ is an alias to /src
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import Token from '@/components/Token.vue'
 
   export default {
     name: 'home',
     components: {
-      HelloWorld
+      'app-token': Token
     }
   }
 </script>
