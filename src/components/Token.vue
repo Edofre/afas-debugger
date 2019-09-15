1a5ae87c05cf10ab8e0255f409ef6e67b9e6c116
Edo Freriks <edofre@gmail.com>
2019-09-15 14:12:53 +0200

    Token form

--- src/components/Token.vue
+++ src/components/Token.vue
@@ -2,18 +2,58 @@
     <div class="border m-6 rounded-lg bg-white mx-auto max-w-lg shadow-lg rounded-lg">
         <div class="sm:flex sm:items-center px-6 py-4">
             <form @submit.prevent="save">
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="id">
-                        AFAS ID
-                    </label>
-                    <input
-                            v-model="id"
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="id"
-                            type="text"
-                            placeholder="AFAS ID"
-                    >
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label
+                                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-first-name"
+                        >
+                            AFAS ID
+                        </label>
+                        <input
+                                v-model="id"
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="grid-first-name"
+                                type="text"
+                                placeholder="Jane"
+                        >
+                    </div>
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-xs font-bold">
+                            <span class="text-gray-700">Environment</span>
+                            <select
+                                    v-model="environment"
+                                    class="form-select mt-2 appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            >
+                                <option v-for="envOption in envOptions" :value="envOption.id">{{ envOption.name }}</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    <div class="w-full px-3">
+                        <p class="text-red-500 text-xs italic">We need your AFAS ID to determine what URL we can send requests to.</p>
+                    </div>
                 </div>
+
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full px-3 mb-6 md:mb-0">
+                        <label
+                                class="block resize-y uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="token"
+                        >
+                            Token
+                        </label>
+                        <textarea
+                                v-model="token"
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="token"
+                                placeholder="Your AFAS token"
+                        ></textarea>
+                        <p class="text-red-500 text-xs italic">Please enter your token.</p>
+                    </div>
+                </div>
+
+
                 <div class="flex items-center justify-between">
                     <button
                             type="submit"
@@ -34,7 +74,12 @@
       return {
         id: null,
         environment: 'test',
-        token: null
+        token: null,
+        envOptions: [
+          { id: 'test', 'name': 'Testing' },
+          { id: 'acceptance', 'name': 'Acceptance' },
+          { id: 'production', 'name': 'Production' }
+        ]
       }
     },
     methods: {
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div class="border m-6 rounded-lg bg-white mx-auto max-w-lg shadow-lg rounded-lg">
         <div class="px-6 py-4">
             <div class="mb-4">
-                <h1 class="text-xl mb-2">This is the about page</h1>
+                <h1 class="text-xl mb-2">About this application</h1>
                 <p>You can use this application to debug your AFAS connectors</p>
             </div>
         </div>
