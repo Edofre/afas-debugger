cdbbd142e482445c2f7679c5707a07dd937833ee
Edo Freriks <edofre@gmail.com>
2019-09-22 22:24:41 +0200

    Stying getconnectors

--- src/views/Connectors/GetConnectors.vue
+++ src/views/Connectors/GetConnectors.vue
@@ -4,8 +4,16 @@
     <div class="px-2 mt-4">
       <div class="flex -mx-2">
         <div class="w-3/12 m-auto px-2 border bg-white shadow-lg rounded-lg">
-          <div class=" h-12">
-            {{ getConnectors }}
+          <h2>Select GetConnector</h2>
+          <div class="border rounded">
+            <div v-for="getConnector in getConnectors" :key="getConnector.id" @click="select(getConnector)" class="border-b p-2 last:border-b-0 hover:bg-afas-blue hover:text-white cursor-pointer">
+              <div class="overflow-hidden font-bold">
+                {{ getConnector.id }}
+                <div class="text-sm font-normal">
+                  {{ getConnector.description }}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="w-8/12 m-auto px-2 border bg-white shadow-lg rounded-lg">
@@ -28,6 +36,11 @@
     computed: {
       getConnectors() {
         return this.$store.getters.getConnectors
+      }
+    },
+    methods: {
+      select(connector) {
+        console.log(connector)
       }
     },
     created() {
