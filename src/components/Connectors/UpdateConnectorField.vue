7e41fd265d752ce9d1f21bb06f9c71e25f01db35
Edo Freriks <edofre@gmail.com>
2019-10-04 20:13:07 +0200

    Hide more details

--- src/components/Connectors/UpdateConnectorField.vue
+++ src/components/Connectors/UpdateConnectorField.vue
@@ -1,46 +1,39 @@
 <template>
   <div @click="toggleDetails" class="border-b p-2 border-afas-blue hover:bg-gray-200 cursor-pointer">
     <div class="sm:flex sm:items-center pb-2 w-full">
-      <div class="md:w-1/6 px-3 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
-          ControlType
-        </label>
-        {{ field.controlType }}
-      </div>
-      <div class="md:w-1/6 px-3 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
-          DataType
-        </label>
-        {{ field.dataType }}
-      </div>
-      <div class="md:w-1/6 px-3 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
-          DecimalFieldId
-        </label>
-        {{ field.decimalFieldId }}
-      </div>
-      <div class="md:w-1/6 px-3 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
-          Decimals
-        </label>
-        {{ field.decimals }}
-      </div>
-      <div class="md:w-1/6 px-3 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
-          FieldId
-        </label>
-        {{ field.fieldId }}
-      </div>
-      <div class="md:w-1/6 px-3 md:mb-0">
+      <div class="md:w-1/2 px-3 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
           Label
         </label>
         {{ field.label }}
       </div>
+
+      <div class="md:w-1/2 px-3 md:mb-0">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold">
+          FieldId
+        </label>
+        {{ field.fieldId }}
+      </div>
     </div>
 
     <div v-if="shouldShowDetails" class="border-t border-grey pt-2">
       <table class="w-1/2 text-left m-2">
+        <tr class="border-b">
+          <td class="w-1/2 uppercase text-right text-xs font-bold">Decimals</td>
+          <td class="w-1/2 pl-4">{{ field.decimals }}</td>
+        </tr>
+        <tr class="border-b">
+          <td class="w-1/2 uppercase text-right text-xs font-bold">DecimalFieldId</td>
+          <td class="w-1/2 pl-4">{{ field.decimalFieldId }}</td>
+        </tr>
+        <tr class="border-b">
+          <td class="w-1/2 uppercase text-right text-xs font-bold">DataType</td>
+          <td class="w-1/2 pl-4">{{ field.dataType }}</td>
+        </tr>
+        <tr class="border-b">
+          <td class="w-1/2 uppercase text-right text-xs font-bold">ControlType</td>
+          <td class="w-1/2 pl-4">{{ field.controlType }}</td>
+        </tr>
         <tr class="border-b">
           <td class="w-1/2 uppercase text-right text-xs font-bold">Length</td>
           <td class="w-1/2 pl-4">{{ field.length }}</td>
