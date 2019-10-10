b63d0c80eb48d2ca8944e429750db6d639b54b71
Edo Freriks <edofre@gmail.com>
2019-10-10 23:43:53 +0200

    Remove debug modal code & start refactoring connectors

--- src/views/Connectors/GetConnectors.vue
+++ src/views/Connectors/GetConnectors.vue
@@ -44,41 +44,9 @@
             <font-awesome-icon class="font-awesome-icon" icon="spinner" spin/>
           </div>
           <div v-else>
-            <div v-if="selectedGetConnector" class="">
-              <div v-if="loadingGetConnectorMetaInfo" class="text-center">
-                <font-awesome-icon class="font-awesome-icon" icon="spinner" spin/>
-              </div>
-              <div v-else>
-                <div class="text-lg mb-3">
-                  {{ getConnectorMetaInfo.name }}
-                  <span class="text-sm italic">
-                    ({{ getConnectorMetaInfo.description }})
-                  </span>
-                  <span class="float-right">
-                    <input type="text" class="appearance-none block text-sm w-full bg-gray-200 text-gray-700 border rounded leading-tight px-2 py-1 focus:outline-none focus:bg-white" placeholder="Search" v-model="search">
-                  </span>
-                </div>
 
-                <template v-if="fields.length">
-                  <div class="border-2 border-afas-blue rounded">
-                    <app-get-connector-field
-                      v-for="field in fields"
-                      :key="field.id"
-                      :field="field"
-                    ></app-get-connector-field>
-                  </div>
-                </template>
-                <template v-else>
-                  <div class="text-center font-bold">
-                    {{ search ? 'No fields found' : 'No fields available' }}
-                    <hr/>
-                  </div>
-                </template>
-              </div>
-            </div>
-            <div v-else class="text-center">
-              {{ 'Please select a connector' }}
-            </div>
+            // Fields
+
           </div>
         </div>
       </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="home">
-
     <div class="px-2 mt-4">
       <div class="flex -mx-2">
         <div class="w-5/12 m-auto px-2 border bg-white shadow-lg rounded-lg">
-          <!--          <button @click="openTesting" class="bg-afas-blue hover:bg-afas-red text-white px-4 py-2 leading-none rounded focus:outline-none focus:shadow-outline">-->
-          <!--            testing-->
-          <!--          </button>-->
-          <!--          <button @click="openOther" class="bg-afas-blue hover:bg-afas-red text-white px-4 py-2 leading-none rounded focus:outline-none focus:shadow-outline">-->
-          <!--            other-->
-          <!--          </button>-->
-
           <app-token v-if="token"></app-token>
           <app-save-token v-else></app-save-token>
         </div>
@@ -21,21 +13,11 @@
         </transition>
       </div>
     </div>
-
-    <!--    <app-modal ref="testing">-->
-    <!--      Testing-->
-    <!--    </app-modal>-->
-
-    <!--    <app-modal ref="other">-->
-    <!--      Other-->
-    <!--    </app-modal>-->
-
   </div>
 </template>
 
 <script>
   import Token from '@/components/Token/Token.vue'
-  import Modal from '@/components/UI/Modal.vue'
   import SaveToken from '@/components/Token/SaveToken.vue'
   import ConnectionStatus from '@/components/Token/ConnectionStatus.vue'
 
@@ -43,12 +25,10 @@
     name: 'home',
     data() {
       return {
-        openModal: false
       }
     },
     components: {
       'app-token': Token,
-      'app-modal': Modal,
       'app-save-token': SaveToken,
       'app-connection-status': ConnectionStatus
     },
@@ -58,14 +38,6 @@
       },
       tokenConnection() {
         return this.$store.getters.tokenConnection
-      }
-    },
-    methods: {
-      openTesting() {
-        this.$refs.testing.openModal()
-      },
-      openOther() {
-        this.$refs.other.openModal()
       }
     }
   }
