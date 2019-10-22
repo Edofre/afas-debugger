c24813de4cb18b69a5f5bc033c733f5e06a87f53
Edo Freriks <edofre@gmail.com>
2019-10-22 20:10:46 +0200

    Refactor types

--- src/store/types.js
+++ src/store/types.js
@@ -20,7 +20,7 @@ export const LOAD_VERSION = 'token/LOAD_VERSION'
 
 /*
 |--------------------------------------------------------------------------
-| AFAS
+| GetConnectors
 |--------------------------------------------------------------------------
 */
 // Mutations
@@ -28,14 +28,24 @@ export const MUTATE_GET_CONNECTORS = 'connectors/MUTATE_GET_CONNECTORS'
 export const MUTATE_LOADING_GET_CONNECTORS = 'connectors/MUTATE_LOADING_GET_CONNECTORS'
 export const MUTATE_GET_CONNECTOR_META_INFO = 'connectors/MUTATE_GET_CONNECTOR_META_INFO'
 export const MUTATE_LOADING_GET_CONNECTOR_META_INFO = 'connectors/MUTATE_LOADING_GET_CONNECTOR_META_INFO'
+
+
+// Actions
+export const LOAD_GET_CONNECTORS = 'connectors/LOAD_GET_CONNECTORS'
+export const LOAD_GET_CONNECTOR_META_INFO = 'connectors/LOAD_GET_CONNECTOR_META_INFO'
+
+/*
+|--------------------------------------------------------------------------
+| UpdateConnectors
+|--------------------------------------------------------------------------
+*/
+// Mutations
 export const MUTATE_UPDATE_CONNECTORS = 'connectors/MUTATE_UPDATE_CONNECTORS'
 export const MUTATE_LOADING_UPDATE_CONNECTORS = 'connectors/MUTATE_LOADING_UPDATE_CONNECTORS'
 export const MUTATE_UPDATE_CONNECTOR_META_INFO = 'connectors/MUTATE_UPDATE_CONNECTOR_META_INFO'
 export const MUTATE_LOADING_UPDATE_CONNECTOR_META_INFO = 'connectors/MUTATE_LOADING_UPDATE_CONNECTOR_META_INFO'
 
 // Actions
-export const LOAD_GET_CONNECTORS = 'connectors/LOAD_GET_CONNECTORS'
-export const LOAD_GET_CONNECTOR_META_INFO = 'connectors/LOAD_GET_CONNECTOR_META_INFO'
 export const LOAD_UPDATE_CONNECTORS = 'connectors/LOAD_UPDATE_CONNECTORS'
 export const LOAD_UPDATE_CONNECTOR_META_INFO = 'connectors/LOAD_UPDATE_CONNECTOR_META_INFO'
 
