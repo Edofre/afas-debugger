3385515da8afa496ff1876e57de9b6328219abfd
Edo Freriks <edofre@gmail.com>
2019-10-25 15:28:20 +0200

    Select filters and remove from fields array

--- src/components/GetConnector/MakeRequest.vue
+++ src/components/GetConnector/MakeRequest.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-if="selectedGetConnector" class="">
     <form class="w-full" @submit.prevent="execute">
+
       <div class="flex flex-wrap mb-2">
+
         <div class="md:w-1/5 px-3">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1"
@@ -40,20 +42,39 @@
           <p v-if="$v.take.$error" class="text-red-500 text-xs italic">Skip can only be a positive field</p>
         </div>
 
-        {{ fields }}
-
-        <div class="md:w-1/5 mt-5">
-          <button
-            :disabled="$v.$invalid"
-            :class="{'opacity-50 cursor-not-allowed': $v.$invalid, 'hover:bg-afas-red': !$v.$invalid}"
-            @click="execute"
-            class="bg-afas-blue text-white py-3 px-4 leading-none rounded focus:outline-none focus:shadow-outline"
-          >
-            <font-awesome-icon class="font-awesome-icon" icon="play"/>
-            Execute
-          </button>
+        <div class="md:w-1/5 px-3">
+          <label class="block uppercase tracking-wide text-xs font-bold">
+            <span class="text-gray-700">Select filter</span>
+            <select
+              v-model="filter"
+              @blur="selectFilter"
+              class="form-select mt-1 appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white"
+            >
+              <option value="" disabled selected>Select filter</option>
+              <option v-for="field in fields" :key="field.id" :value="field.id">{{ field.label }}</option>
+            </select>
+          </label>
         </div>
       </div>
+
+      <div v-if="filters.length > 0" class="flex flex-wrap mb-2">
+        {{ filters }}
+      </div>
+
+
+      <div class="md:w-1/5 mt-5">
+        <button
+          :disabled="$v.$invalid"
+          :class="{'opacity-50 cursor-not-allowed': $v.$invalid, 'hover:bg-afas-red': !$v.$invalid}"
+          @click="execute"
+          class="bg-afas-blue text-white py-3 px-4 leading-none rounded focus:outline-none focus:shadow-outline"
+        >
+          <font-awesome-icon class="font-awesome-icon" icon="play"/>
+          Execute
+        </button>
+      </div>
+
+
     </form>
 
     <div v-if="!executingGetConnector">
@@ -88,7 +109,10 @@
     data() {
       return {
         skip: 0,
-        take: 100
+        take: 100,
+        filter: null,
+        filters: [],
+        directions: ['asc', 'desc']
       }
     },
     computed: {
@@ -103,7 +127,18 @@
       },
       fields() {
         if (this.getConnectorMetaInfo) {
-          return this.getConnectorMetaInfo.fields
+          let fields = this.getConnectorMetaInfo.fields
+
+          if (this.filters.length > 0) {
+
+            let filters = this.filters
+
+            fields = fields.filter(x => {
+              return filters.findIndex(t => t.id === x.id) === -1
+            })
+          }
+
+          return fields
         }
         return []
       }
@@ -119,6 +154,12 @@
       }
     },
     methods: {
+      selectFilter() {
+        let filter = this.filter
+        let filterObject = this.fields.find(x => x.id === filter)
+        this.filters.push(filterObject)
+        this.filter = null // Reset the filter
+      },
       execute() {
         this.$store.dispatch(EXECUTE_GET_CONNECTOR, {
           token: JSON.parse(localStorage.getItem('afas_token')),
